62e0b37b0a20adc9b10957bb0e8a2686bd0013ba
kaneriya vasu mukeshbhai <vasu@kaneriyas-MacBook-Air.local>
2025-12-04 23:34:08 +0530

    next js file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 }
 
